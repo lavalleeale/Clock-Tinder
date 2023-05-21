3e00a6d0634a5d2d487c6e2765bca800b313139e
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-05-21 07:10:49 -0700

    Initial Commit

---
+++ Clock Tinder/Clock_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_TinderApp.swift
+//  Clock Tinder
+//
+//  Created by Alex lavallee on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Clock_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
